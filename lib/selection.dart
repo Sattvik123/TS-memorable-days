817a72a1c33ef3081e199fb3dc08ab965d731af7
Sattvik123 <sattvik.gupta@ais.amity.edu>
2022-05-20 17:18:17 +0530

    edited selection page file

--- lib/selection.dart
+++ lib/selection.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shbc/login.dart';
+import 'package:shbc/register.dart';
 
 class SelectionScreen extends StatefulWidget {
   const SelectionScreen({Key? key}) : super(key: key);
@@ -11,16 +13,28 @@ class _SelectionScreenState extends State<SelectionScreen> {
   @override
   Widget build(BuildContext context) {
     final navToLogin = Material(
+      color: Colors.green,
       elevation: 5,
+      borderRadius: BorderRadius.circular(15),
       child: MaterialButton(
-        onPressed: () => {},
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: () => {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const LoginScreen()))
+        },
         child: const Text('Login'),
       ),
     );
     final navToRegister = Material(
+      color: Colors.blue,
       elevation: 5,
+      borderRadius: BorderRadius.circular(15),
       child: MaterialButton(
-        onPressed: () => {},
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: () => {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const RegisterScreen()))
+        },
         child: const Text('Register'),
       ),
     );
@@ -29,9 +43,14 @@ class _SelectionScreenState extends State<SelectionScreen> {
       appBar: AppBar(
         title: const Text('Select Login/Register'),
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(children: [navToLogin, navToRegister]),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(15),
+          child: Column(children: [
+            navToLogin,
+            const Padding(padding: EdgeInsets.all(15)),
+            navToRegister,
+          ]),
         ),
       ),
     );
