7bb6808218bc4a994f983deaa1f566c9cadac5d5
Sattvik123 <sattvik.gupta@ais.amity.edu>
2022-05-21 20:02:29 +0530

    edited

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:shbc/selection.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 void main() {
   runApp(const MyApp());
 }
