51a2ae5ef9c5cc89a972fe288da06d0eab14f423
Sattvik123 <sattvik.gupta@ais.amity.edu>
2022-05-23 12:09:39 +0530

    edited

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:shbc/selection.dart';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
