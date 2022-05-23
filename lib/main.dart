68b8b651375cabd06e6584248aa4472fcf7992de
Sattvik123 <sattvik.gupta@ais.amity.edu>
2022-05-23 12:22:01 +0530

    edited

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:shbc/selection.dart';
-
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
